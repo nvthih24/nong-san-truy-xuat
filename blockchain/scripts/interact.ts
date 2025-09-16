405d4d6e2b9d3f2f1f83e1cd4f5e79e606d5bee9
PhamNguyenCaoKy_2280601671 <phamnguyencaoky0605@gmail.com>
2025-09-16 18:35:24 +0700

    Upload source code

---
+++ blockchain/scripts/interact.ts
@@ -0,0 +1,39 @@
+import { ethers } from "ethers";
+import CounterArtifact from "../artifacts/contracts/Counter.sol/Counter.json";
+
+async function main() {
+  // Provider kết nối node local của Hardhat
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+
+  // Lấy danh sách account từ node local
+  const accounts = await provider.listAccounts();
+  console.log("Accounts:", accounts);
+
+  // signer = account đầu tiên
+  const signer = await provider.getSigner(0);
+
+  // Địa chỉ contract sau khi bạn deploy Counter
+  const contractAddress = "0x2546bcd3c84621e976d8185a91a922ae77ecec30 ";  // ⚠️ thay bằng địa chỉ deploy thực tế
+
+  // Lấy ABI từ Counter.json
+  const abi = CounterArtifact.abi;
+
+  // Tạo contract instance
+  const counter = new ethers.Contract(contractAddress, abi, signer);
+
+  // Gọi thử các hàm trong Counter
+  const current = await counter.getCount();
+  console.log("Current count:", current.toString());
+
+  console.log("Sending tx to increment...");
+  const tx = await counter.increment();
+  await tx.wait();
+
+  const updated = await counter.getCount();
+  console.log("Updated count:", updated.toString());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
