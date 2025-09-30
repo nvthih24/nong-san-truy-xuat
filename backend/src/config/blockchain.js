0b251a63d7229bd12ed1dc868c4980a567f87371
Baozxje <lamgiabao73@gmail.com>
2025-09-30 13:33:38 +0700

    new mew

--- backend/src/config/blockchain.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require('ethers');
-require('dotenv').config();
-
-const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
-module.exports = { provider, wallet };
